c0cb3aa568a5f11f3b611d4a3305f39b87f1da3d
ericafenyo <ericafenyo@yahoo.com>
2023-10-06 09:36:50 +0200

    add few classes for algo

--- src/main/java/playground/Main.java
+++ src/main/java/playground/Main.java
@@ -1,7 +1,12 @@
 package playground;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-
+        for (int x = 0; x > 10; x++) {
+            System.out.println("sged");
+        }
     }
 }
